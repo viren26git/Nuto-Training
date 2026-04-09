e1326c91eff9d227783fbd1c56d47ed50b1fa4c5
viren26git <64171510+viren26git@users.noreply.github.com>
2026-04-09 17:20:25 +0530

    Add files via upload

--- validate.js
+++ validate.js
@@ -3,3 +3,16 @@ function show()
         document.body.style.backgroundColor ="orange";
         alert('Form submitted !!')
 }
+
+//-----------------------//
+//let e1 = document.getElementById("div1")
+//e1.style.color="red";
+
+//----------------------------//
+let items = document.getElementsByClassName("class1");   // 3
+items[0].style.color = "blue";
+items[1].style.color= "white"
+
+//--------------------//
+
+
